28db3089f096a2a4551cde02e5c847c3ab109c79
Alex <o.a.momotyuk@gmail.com>
2019-10-25 15:56:27 -0500

    wrote EXIF GPS data to degree convert function

--- src/components/upload/Upload.js
+++ src/components/upload/Upload.js
@@ -3,6 +3,7 @@ import React, { Component } from "react"
 import { Button, Form, FormGroup, Input, Card, CardBody, Row, Col } from 'reactstrap'
 //import { Label } from 'reactstrap'
 //import ImageUploader from 'react-images-upload';
+import GPStoDegree from "./GPStoDegree"
 
 //
 var EXIF = require("../../../node_modules/exif-js/exif.js");
@@ -37,50 +38,16 @@ class Upload extends Component {
         EXIF.getData(event.target.files[0], function () {
             var allMetaData = EXIF.getAllTags(this);
             console.log("metaData:", allMetaData)
-
-            console.log("DateTime:", allMetaData.DateTime)
-            console.log("GPSLatitude:", allMetaData.GPSLatitude)
-            console.log("GPSLatitudeRef:", allMetaData.GPSLatitudeRef)
-            console.log("GPSLongitude:", allMetaData.GPSLongitude)
-            console.log("GPSLongitudeRef:", allMetaData.GPSLongitudeRef)
             /*
-                "photos": [
-        {
-            "id": 1,
-            "fileName": "photo-191018-0950-01.jpg",
-            "takenDate": "2019-10-18T09:50:00.000Z",
-            "uploadDate": "2019-10-18T09:51:00.000Z",
-            "latitude": "36.116753 N",
-            "longitude": "86.719244 W",
-            "comment": "No comment",
-            "userId": 1
-        },
+                        console.log("DateTime:", allMetaData.DateTime)
+                        console.log("GPSLatitude:", allMetaData.GPSLatitude)
+                        console.log("GPSLatitudeRef:", allMetaData.GPSLatitudeRef)
+                        console.log("GPSLongitude:", allMetaData.GPSLongitude)
+                        console.log("GPSLongitudeRef:", allMetaData.GPSLongitudeRef)
+            */
+            var Convertor = new GPStoDegree(allMetaData)
 
-            */
-            /*
-            EXIF stores GPS coords as rational64u which is a list of six unsigned whole numbers in the following order:
-            [
-               degreesNumerator, degreesDenominator,
-               minutesNumerator, minutesDenominator,
-               secondsNumerator, secondsDenominator
-            ]
-            The format is consistent, and it looks like the tool you are using has already divided each pair into decimal numbers, so what you have is:
-            Lat: 55°   40.711' 0"
-            Lng:  8°  30.2282' 0"
-            If you want to convert to a single decimal number:
-            = Degrees + Minutes/60 + Seconds/3600
-            */
-            /*
-            GPSLatitude: (3) [Number, Number, Number]
-            GPSLatitudeRef: "N"
-            GPSLongitude: (3) [Number, Number, Number]
-            GPSLongitudeRef: "W"
-            */
-            //var allMetaDataSpan = document.getElementById("allMetaDataSpan");
-            //            allMetaDataSpan.innerHTML = JSON.stringify(allMetaData, null, "\t");
-
-            //var allMetaDataSpanJson = JSON.stringify(allMetaData, null, "\t");
-            //console.log("Upload.handleUpload: allMetaDataSpanJson:", allMetaDataSpanJson)
+            console.log("GPS degree data:", Convertor.getData())
         });
     }
 
